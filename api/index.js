793c28baa91cb380bf133404d9c219c5919b4821
Abrahem Alhofe <alhofeabrahem88@gmail.com>
2024-12-25 20:51:59 +0200

    fix ffprobe

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getAudioDurationInSeconds } from 'get-audio-duration';
+import { getAudioDurationInSeconds } from 'ffprobe/index.js';
 
 const app = express();
 
